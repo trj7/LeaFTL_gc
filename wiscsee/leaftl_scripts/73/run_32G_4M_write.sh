be1fd45db81bd2aed609c8a851c0a26c946537f0
WLeoo <3049458366@qq.com>
2026-01-15 23:58:01 +0800

    feature:8G|32G

---
+++ wiscsee/leaftl_scripts/73/run_32G_4M_write.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+# 固定脚本所在目录（关键）
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
+# PATH
+export PATH="$PATH:$SCRIPT_DIR/../pypy2.7-v7.3.9-linux64/bin"
+
+# 输出目录
+OUT_DIR="$SCRIPT_DIR/../raw_results/memory_batch/73"
+
+# 确保输出目录存在 (可选)
+# mkdir -p "$OUT_DIR"
+
+# 执行
+pypy "$SCRIPT_DIR/../run_ftl" \
+    -sl 0 \
+    -l 1000000000 \
+    -t "$SCRIPT_DIR/../leaftl_traces/32G/write_28800M_zoned73_64G_200ns_shuffle_precond.trace" \
+    -c 8 \
+    -mc 4096 \
+    -f learnedftl \
+    -wo 0 \
+    -q 1 \
+    -p 4096 \
+    -o "$OUT_DIR/test_32G_4M" \
+    -cf "$SCRIPT_DIR/../config/32G.json" \
+    >> "$OUT_DIR/test_32G_1M_stdout_write.txt" 2>&1
---
+++ wiscsee/leaftl_scripts/82/run_8G_1M_read.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+# 固定脚本所在目录（关键）
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
+# PATH
+export PATH="$PATH:$SCRIPT_DIR/../pypy2.7-v7.3.9-linux64/bin"
+
+# 输出目录
+OUT_DIR="$SCRIPT_DIR/../raw_results/memory_batch/82"
+
+# 确保输出目录存在 (可选)
+# mkdir -p "$OUT_DIR"
+
+# 执行
+pypy "$SCRIPT_DIR/../run_ftl" \
+    -sl 0 \
+    -l 1000000000 \
+    -t "$SCRIPT_DIR/../leaftl_traces/8G/read_7200M_zoned82_16G_200ns_shuffle_precond.trace" \
+    -c 8 \
+    -mc 1024 \
+    -f learnedftl \
+    -wo 0 \
+    -q 1 \
+    -p 4096 \
+    -o "$OUT_DIR/test_8G_1M" \
+    -cf "$SCRIPT_DIR/../config/8G.json" \
+    >> "$OUT_DIR/test_8G_1M_stdout_read.txt" 2>&1
