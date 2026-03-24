adedc0434f79a8f733e8cf20fe0eb151122fd83a
WLeoo <3049458366@qq.com>
2026-03-24 15:41:58 +0800

    feature:aliyun

---
+++ wiscsee/leaftl_scripts/91/altrace_80.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+# 检查参数
+if [ -z "$1" ]; then
+    echo "Usage: $0 <trace_file>"
+    exit 1
+fi
+
+TRACE_FILE="$1"
+TRACE_NAME=$(basename "$TRACE_FILE" .trace)
+
+# 固定脚本所在目录
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
+# PATH
+export PATH="$PATH:$SCRIPT_DIR/../pypy2.7-v7.3.9-linux64/bin"
+
+# 输出目录
+OUT_DIR="$SCRIPT_DIR/../raw_results/memory_batch/altrace_80"
+
+# 执行
+pypy "$SCRIPT_DIR/../run_ftl" \
+    -sl 0 \
+    -l 1000000000 \
+    -t "$TRACE_FILE" \
+    -c 8 \
+    -mc 5120 \
+    -f learnedftl \
+    -wo 0 \
+    -q 1 \
+    -p 4096 \
+    -o "$OUT_DIR/${TRACE_NAME}"\
+    -cf "$SCRIPT_DIR/../config/80G.json" \
+    >> "$OUT_DIR/${TRACE_NAME}_stdout.txt" 2>&1
