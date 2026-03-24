c0d8b12b8b26a5f7ae3fe6bb621cc5c84df2429e
WLeoo <3049458366@qq.com>
2026-03-24 15:45:19 +0800

    feature:setup.sh

--- wiscsee/leaftl_scripts/setup.sh
+++ wiscsee/leaftl_scripts/setup.sh
@@ -1,4 +1,4 @@
 export PATH=$PATH:pypy2.7-v7.3.9-linux64/bin
-mkdir -p raw_results/memory_batch/91 raw_results/memory_batch/82 raw_results/memory_batch/73 raw_results/memory_batch/64 plots
+mkdir -p leaftl_traces/aliyuntest_1T raw_results/memory_batch/91 raw_results/memory_batch/82 raw_results/memory_batch/73 raw_results/memory_batch/64 plots
 pip3 install psutil simpy bitarray bidict
 pypy -m pip install wheel scipy objgraph
